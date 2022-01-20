1d72ca8076d0fe00a8a6e56f22a27b14ed005516
Aaron Kirchhoff <askirchhoff@gmail.com>
2022-01-20 07:16:04 -0800

    alert message working

--- src/star.js
+++ src/star.js
@@ -1,34 +1,56 @@
 import React, {Component} from 'react';
 
 class Star extends Component {
-  static defaultProps = {
-
-  }
 
   constructor(props){
     super(props);
     // state is how much mass the star is, we will set it at 0 for now, update state with an input field, and based on that, we call a function that will decide what star to display.
     this.state = {star: ''};
     this.handleChange = this.handleChange.bind(this);
-    this.tbdfunction = this.tbdfunction.bind(this);    
+    this.tbdfunction = this.tbdfunction.bind(this);   
+    this.formSubmit = this.formSubmit.bind(this); 
   }
 
   // this function is what captures input from out text box to change the state. target is the value passed along from when we fill out the field on the UX side.
   handleChange(mass) {
+    // runs on every keystroke, it's called a controlled component because react is following along with every entry the user makes in real time. beacuse of the onChange() method below
     this.setState({ star: mass.target.value });
   }
 
   formSubmit(evt){
-    this.tbdfunction();
-    evt.preventDefault();
-
+    var starType;
+    if( this.state.star === ''){
+      starType = '';
+    } else if (this.state.star <= 3) {
+      starType = 'Protostar';
+    } else if (this.state.star <= 6) {
+      starType = 'T Tauri';
+    } else if (this.state.star <= 10) {
+      starType = 'Red Dwarf';
+    } else if (this.state.star <= 11) {
+      starType = 'White Dwarf';
+    } else if (this.state.star <= 20) {
+      starType = 'Red Giant';
+    } else if (this.state.star <= 35) {
+      starType = 'Neutron Star';
+    } else if (this.state.star <= 100) {
+      starType = 'Super Giant';
+    } else {
+      starType = 'error: Mass input incorrect'
+    }
+    alert(`you typed: ${starType}`);
+    this.setState({star: ''})
+    return starType
   }
+  
 
 
   tbdfunction(){
     // and we dont want tpage t refresh automatically, so ...
     var starType;
-    if( this.state.star <= 3){
+    if( this.state.star === ''){
+      starType = '';
+    } else if (this.state.star <= 3) {
       starType = 'Protostar';
     } else if (this.state.star <= 6) {
       starType = 'T Tauri';
@@ -48,9 +70,10 @@ class Star extends Component {
     return starType
 
   }
-// leave note, working with evt.preventDefault(), with it active my screen goes blank, maybe on the right track, when i hit the submit button it resets State back to 0, right now, filling out the input field updates state as i go, and i dont want that.
+
   render(){    
-    const newStar = this.tbdfunction();
+    // leave note: no idea what to put here to bring in starType variable
+    const newStar = this.state.star;
     return (
       <div className='Star'>
         <h1> Check my Universe | {this.state.star}</h1>
@@ -59,7 +82,8 @@ class Star extends Component {
           <label>Mass: 
             <input type="text" id="mass" value={this.state.star} onChange={this.handleChange}></input>
           </label>
-          <input type="submit" value="Submit" />
+          {/* <input type="submit" value="Submit" /> */}
+          <button>Submit here</button>
         </form >
         <h1> {newStar} </h1>
 
