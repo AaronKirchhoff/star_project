2dd829520e387ed80e9eb561bf628322b14f3580
Aaron Kirchhoff <askirchhoff@gmail.com>
2022-01-24 18:14:10 -0800

    more bootstrap, mvp list

--- src/App.js
+++ src/App.js
@@ -12,7 +12,20 @@ function App() {
       <div className='square'> ☀</div>
       </header>
       <Star />
-      <button type="button" class="btn btn-primary">Hey here is bootstrap button</button>
+      <div className='To-do'>
+        <h4 class="display-4 text-info" >To Do list | Minimal Viable Product</h4>
+        <ul>
+          <li>space api, astronomy picture of the day</li>
+          <li>input mass and luminosity to calculate actual star class</li>
+          <li>quis nostrud exercitation ullamco laboris</li>
+          <li>nisi ut aliquip ex ea commodo consequat.</li>
+        </ul>        
+      </div>
+      <div>
+        <button type="button" class="btn btn-primary">Hey here is bootstrap button</button>
+        <button type="button" class="btn btn-danger">delete star</button>
+      </div>
+      
       
       
 
