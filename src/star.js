e2a486f43814db08ac72c9e08d0af51ecbcbd9d5
Aaron Kirchhoff <askirchhoff@gmail.com>
2022-02-02 05:39:07 -0800

    added more states to change

--- src/star.js
+++ src/star.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import StarType from './StarType';
 import theSun from "./the-sun.jpg";
+import milkyWay from "./milky-way.jpg"
 
 
 // star component is the container for StarType, this is where im going to do all the heavy lifting and run my methods. I will pass along some state info as props to StarType and call that.
@@ -10,9 +11,11 @@ class Star extends Component {
     super(props);
     // state is how much mass the star is, we will set it at 0 for now, update state with an input field, and based on that, we call a function that will decide what star to display.
     this.state = {star: ''};
-    // currentState is saved at starType variable below
+    // currentStar is the name of the star
     this.state = {currentStar: ''};
-    this.state = {defaultImageState: theSun};
+    this.state = {defaultImageState: milkyWay};
+    // leave note: the starBio is causing an error and my default props wont show???? hmmm...
+    // this.state = {starBio: ''};
     this.handleChange = this.handleChange.bind(this);
     this.formSubmit = this.formSubmit.bind(this); 
   }
@@ -25,10 +28,14 @@ class Star extends Component {
 
   formSubmit(evt){
     var starType;
+    var starPic;
+    // var starInformation;
     if( this.state.star === ''){
       starType = '';
     } else if (this.state.star <= 3) {
       starType = 'Protostar';
+      starPic = theSun;
+      // starInformation = "A protostar is a very young star that is still gathering mass from its parent molecular cloud. The protostellar phase is the earliest one in the process of stellar evolution.[1] For a low-mass star (i.e. that of the Sun or lower), it lasts about 500,000 years.[2] The phase begins when a molecular cloud fragment first collapses under the force of self-gravity and an opaque, pressure supported core forms inside the collapsing fragment. It ends when the infalling gas is depleted, leaving a pre-main-sequence star, which contracts to later become a main-sequence star at the onset of hydrogen fusion producing helium.";
     } else if (this.state.star <= 6) {
       starType = 'T Tauri';
     } else if (this.state.star <= 10) {
@@ -45,6 +52,8 @@ class Star extends Component {
       starType = 'error: Mass input incorrect'
     }
     this.setState({currentStar: starType})
+    this.setState({defaultImageState: starPic})
+    // this.setState({starBio: starInformation})
     evt.preventDefault();
     // alert(`you typed: ${starType}`);
     this.setState({star: ''})
@@ -63,7 +72,7 @@ class Star extends Component {
           <button class="btn btn-success">Calculate</button>
         </form >
         {/* passing a prop to StarType compenent. super simple. i even passed the currentStar state as a prop. can't figure out how to set an image as adeault prop in StarType... so for now, always put in the prop for starImage as the current state of defaultIMageState from here.*/}
-        <StarType starImage={this.state.defaultImageState} name={this.state.currentStar}/>
+        <StarType starImage={this.state.defaultImageState} name={this.state.currentStar} data={this.state.starBio}/>
 
       </div>
       
