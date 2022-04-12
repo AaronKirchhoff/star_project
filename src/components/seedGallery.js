6fe9daa5f5d661375fca68655a544bb70202adc7
Aaron Kirchhoff <askirchhoff@gmail.com>
2022-04-12 09:35:20 -0700

    galaxy gallery photos added

---
+++ src/components/seedGallery.js
@@ -0,0 +1,35 @@
+import "../pictures/SpaceGalleryPhotos"
+
+export default [
+  {
+    galleryName: "Galaxies",
+    id: "galaxies",
+    emoji: "🌌",
+    photos: [
+      {name: "Galaxy1", pic:"2048px-Milky_Way_Galaxy.jpg"},
+      {name: "Galaxy2", pic:"A_menagerie_of_galaxies.jpg"},
+      {name: "Galaxy3", pic:"Abell-2744-Hubble-Frontier-Field-sq.jpg"},
+      {name: "Galaxy4", pic:"Andromeda.jpg"},
+      {name: "Galaxy5", pic:"andromeda2"},
+      {name: "Galaxy6", pic:"artists-illustration-of-a-spiral-galaxy.jpg"},
+      {name: "Galaxy7", pic:"darkrift-orig_full.jpg"},
+      {name: "Galaxy8", pic:"galaxy-trees.jpg"},
+      {name: "Galaxy9", pic:"galaxy2.jpg"},
+      {name: "Galaxy10", pic:"galaxy3.jpg"},
+      {name: "Galaxy11", pic:"galaxy4.jpg"},
+      {name: "Galaxy12", pic:"GettyImages-galaxy.jpg"},
+      {name: "Galaxy13", pic:"iStock-513298210-feature.jpg"},
+      {name: "Galaxy14", pic:"lots-of-galaxies.jpg"},
+      {name: "Galaxy15", pic:"man-stand-on-a-background-of-a-milky-way.png"},
+      {name: "Galaxy16", pic:"Milkomeda_collison.jpg"},
+      {name: "Galaxy17", pic:"Milky-Way-1200x800.jpg"},
+      {name: "Galaxy18", pic:"oldest-galaxy-in-the-Universe.jpg"},
+      {name: "Galaxy19", pic:"potw1922a-1280x720.jpg"},
+    ]
+
+
+  }
+
+
+
+]
